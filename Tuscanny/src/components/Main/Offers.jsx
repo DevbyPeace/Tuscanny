1e5ecc4ebb3ffa2b4aaf6122a283216fe13d9751
Peace <devbypeace@gmail.com>
2025-08-31 04:37:17 +0100

    new update

--- Tuscanny/src/components/Main/Offers.jsx
+++ Tuscanny/src/components/Main/Offers.jsx
@@ -1,16 +1,18 @@
 import woman from "../../assets/section.svg";
+import "./Offers.css";
 
 function Offers() {
   return (
     <div className="offers-container">
       <div className="offers">
-        <h2> Get Special Offers for Organizations </h2>
+        <h1> Get Special Offers for Organizations </h1>
         <p>
           Check out our latest offers and discounts! We offer exclusive deals
           for organizations looking to book group tours or events in Tuscany.
           Learn more about our packages and how we can customize your
           experience.
         </p>
+        <button>Contact Us</button>
       </div>
 
       <img src={woman} alt="Special Offers" />
